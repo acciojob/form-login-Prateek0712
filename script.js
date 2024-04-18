4274c378668d7e567a059ecfd208af8ca267991d
Prateek Jadhav <134202260+Prateek0712@users.noreply.github.com>
2024-04-18 12:41:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function getFormvalue() {
     //Write your code here
-
+	let loginForm=document.querySelector("form");
+	let fname=loginForm[0].value;
+	let lname=loginForm[1].value;
+	alert(fname+" "+lname);
 }
